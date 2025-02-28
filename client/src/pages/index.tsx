0ae2564e95cda27bed5f82e374ddcec4cfc0419e
conthom <conthom@iu.edu>
2025-02-28 13:45:23 -0500

    light mode fix?

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default function Index() {
     return (
-        <div style={{ overflowX: 'hidden' }} className="text-black h-screen">
+        <div style={{ overflowX: 'hidden' }} className="h-screen">
             <Head>
             <title>Connor Thompson</title>
             <link rel="icon" href="/connor logo.png" />
